aa65a9c2e9d2f9995cfcc5fae4201f3c49dadfb5
Ebrahim <efeghhi@gmail.com>
2024-11-07 21:35:21 -0800

    good night

--- run_untrained_banded.py
+++ run_untrained_banded.py
@@ -3,11 +3,9 @@ sys.path.append('/home2/ebrahim/beyond-brainscore/generate_activations/')
 from LLM import generate_activations_LLM
 from banded_reg_func import himalaya_regression_caller
 
-datasets = ['pereira', 'fedorenko', 'blank']
+datasets = ['pereira', 'fedorenko']
 models = ['gpt2-xl-untrained', 'gpt2-xl-untrained-sp', 'gpt2-xl-untrained-mp']
-linear_reg_options = [False] # if False, do L2 regularized only
-shuffled_options = [True, False]
-model_name = 'gpt2-xl'
+shuffled_options = [False]
 start = 0
 N = 5
 save_y_hat = True
@@ -17,32 +15,33 @@ device = 1
 
 for d in datasets:
     
-    for i in range(start, N):
+    for m in models:
         
-        generate_activations_LLM(dataset=d, model_str=model_name, untrained=True, model_num=f"{i}", decontext=False)
-
-        for m in models:
+        for shuffled in shuffled_options:
             
-            model = f"{m}"
-    
-            for shuffled in shuffled_options:
+            for i in range(start, N):
         
                 if d == 'pereira':
                     exp_options = ['384', '243']
                 else:
                     exp_options = ['']
                     
-                model_seed = f"{model}_m{i}"
-                    
+                if d == 'pereira':
+                    model_name = f"{m}_m{i}_SP+SL"
+                    f_list = [1600, 5]
+                else:
+                    model_name = f"{m}_m{i}_WP"
+                    f_list = [1600, 9]
+                
                 for exp in exp_options:
                     
-                    print(f"Running model {model}, experiment {exp}, dataset {d}, shuffled {shuffled}, save_y_hat {save_y_hat}")
+                    print(f"Running model {model_name}, experiment {exp}, dataset {d}, shuffled {shuffled}, save_y_hat {save_y_hat}")
                     
-                    or2 = himalaya_regression_caller(model=model_seed, y='', data_labels='', features_list=[], 
-                        n_iter=1, dataset=d, data_folder=data_folder, exp=exp, 
+                    or2 = himalaya_regression_caller(model=model_name, y='', data_labels='', features_list=f_list, 
+                        n_iter=1000, dataset=d, data_folder=data_folder, exp=exp, 
                         save_results=True, save_y_hat=save_y_hat, save_new=False, 
                         device=device, untrained=True, linear_reg=False, shuffled=shuffled, 
                         approx_linear=False) 
 
                     
-                        
+                    
